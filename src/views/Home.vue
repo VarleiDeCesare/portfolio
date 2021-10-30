af833fdb065ccf3a760b69945462caa19d767bfb
VarleiDeCesare <varleidecesare@yahoo.com.br>
2021-10-30 11:58:15 -0300

    Criação de páginas e listagem de projetos * Foi colocado os links nos próprios v-tab, com a diretiva "to", assim foi tirado os links do view router do conteúdo do v-tab. * Foi colocado o router-view dentro do templato do App.vue, agora os componentes serão carregados nessa área. * Foi comentado o botão do meu portfolio, para acessar agora é apenas pelo menu superior. * Foi retirado o array de links do data(), agora é simplemente duplicado o v-tab, sem o v-for. * Foi alterado a cor padrão do site, agora por padrão é black, caso o usuário queria mudar vai poder alterar pelo botão do menu. * Foi excluido a rota da página about, e foi exclúido o docmento About.vue. * Foi criado a view Home.vue que é o componente da página principal do site. * Foi criado o arquivo Portfolio.vue que é carregado quando o usuário vai para a página do portfolio a partir do link do menu. * Portfolio.vue contém a listagem de projetos já feitos, para a reprodução de um novo projeto tem um comentário para seguir o passo-a-passo.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,111 @@
 <template>
-  <hello-world />
+	<div>
+		<div class="row">
+			<div class="col">
+				<div class="d-flex justify-content-around">
+					<div id="name-cargo">
+						<h4>Varlei De Cesare</h4>
+						<p>Full-Stack Developer (Laravel/Vue.js/WordPress/PHP)</p>
+					</div>
+					<div>
+						<!--todo aqui tem que colocar uma foto minha-->
+						<!--<img src="" alt="">-->
+						<!--						<i class="fas fa-user fa-3x"></i>-->
+						<v-avatar height="75px" width="75px">
+							<img src="../assets/perfil.jpeg">
+						</v-avatar>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col">
+				<h5 id="title-works">Sobre mim</h5>
+				<p id="about-me">&nbsp;&nbsp;&nbsp;&nbsp;Sou desenvolvedor Full-Stack, trabalhando com as tecnologias Laravel PHP e Vue.js. Atualmente trabalho como terceiro para a empresa Ejetar na criação de sites com painéis administrativos. Sites criados com HTML/5 CSS/3 e Bootstrap para lidar com a responsividade, painel para administrar as informações do site feito com WordPress.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col">
+				<h5 id="title-bio">Bio</h5>
+				<ul id="me-bio">
+					<li><i class="fas fa-map-marker-alt fa-lg link"></i> Água Santa, Rio Grande do Sul - Brasil</li>
+					<li><i class="fas fa-university fa-lg link" style="margin-left: -4px"></i> 4° Semestre de Ciências da Computação - Imed</li>
+					<li><i class="fas fa-code-branch fa-lg link"></i> Experiência como desenvolvedor FullStack - Ejetar</li>
+				</ul>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col">
+				<h5 id="title-social-media">Redes Sociais</h5>
+				<ul id="me-social-media">
+					<li>
+						<a href="https://github.com/VarleiDeCesare"><i class="fab fa-github fa-lg link"></i></a>
+						@VarleiDeCesare
+					</li>
+
+					<li>
+						<a href="https://www.instagram.com/varlei_cesare/"><i class="fab fa-instagram fa-lg link"></i></a>
+						@varlei_cesare
+					</li>
+					<li>
+						<a href="wa.me/5554991091980"><i class="fab fa-whatsapp fa-lg link"></i></a>
+						Varlei De Cesare
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+export default {
+	name: 'Home',
 
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
+	components: {},
+	data(){
+		return {
+			cor_letras: "black"
+		}
+	}
+}
 </script>
+<style>
+:root{
+	--laranja: #e59447
+}
+.link{
+	color: var(--laranja)
+}
+
+#title-welcome {
+	margin: 50px 0;
+	padding: 13px 100px;
+	border-radius: 10px;
+	font-weight: unset;
+}
+
+#title-works,
+#title-bio,
+#title-social-media{
+	margin-left: 50px;
+}
+
+#about-me,
+#me-bio,
+#me-social-media{
+	margin: 20px 0 0 55px;
+	font-size: 17px;
+}
+ul{
+	list-style: none!important;
+}
+li{
+	font-size: 17px;
+	margin-bottom: 3px;
+}
+
+.link-portfolio {
+	margin-top: 20px;
+}
+
+</style>
---
+++ src/views/Portfolio.vue
@@ -0,0 +1,220 @@
+<template>
+	<div>
+		<v-container>
+			<v-row justify="center" style="margin-top: 20px">
+				<h6>Projetos</h6>
+			</v-row>
+			<v-row justify="center">
+				<!--fixme Para cada novo post, duplique uma dessas colunas, troque os dados, troque a variavel dialog e declare a mesma no data()-->
+				<!--Painel Ejetar-->
+				<v-col cols="6">
+					<v-card>
+						<v-img src="../assets/painel.png" max-height="125" contain></v-img>
+						<v-card-title class="text-h6">
+							<v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+								<template v-slot:activator="{ on, attrs }">
+									<div class="data-project">
+										<h5 class="title-project" v-bind="attrs" v-on="on">Painel Ejetar
+											<i class="fas fa-expand-alt fa-sm"></i>
+										</h5>
+										<p class="text-project">Painel para monitorar consumo de Banda e Disco Rígido de clientes do servidor, integração com pagamento</p>
+									</div>
+
+								</template>
+								<div class="content-post-project-father">
+									<v-toolbar dark color="#e59447">
+										<v-btn icon dark @click="dialog = false">
+											<v-icon>mdi-close</v-icon>
+										</v-btn>
+									</v-toolbar>
+									<v-container>
+										<v-row justify="center">
+											<v-col cols="7">
+												<div class="d-flex justify-content-around content-post-project">
+													<div>
+														<h4>Painel Grupo Ejetar</h4>
+														<div class="ml-2">
+															<h6 class="d-flex">
+																<i class="fas fa-laptop"></i>&nbsp;Windows/MacOS/Linux/iOS/Android
+															</h6>
+															<h6 class="d-flex">
+																<i class="far fa-keyboard"></i>&nbsp;Laravel, Vue.js, PHP
+															</h6>
+															<h6 class="d-flex">
+																<i class="fas fa-external-link-alt"></i>&nbsp;painel.grupoejetar.com.br
+															</h6>
+														</div>
+													</div>
+													<div class="text-end about-project">
+														<p>O Painel tem o propósito de captar consumos de Disco e de Banda Larga de cada cliente, fazer o cálculo levando em consideração os gastos e lançar uma cobrança na Stripe. Stripe é a integração na qual a Stack Laravel Cashier lança a cobrança e envia e-mail com o boleto para o cliente poder fazer o pagamento</p>
+													</div>
+												</div>
+											</v-col>
+										</v-row>
+										<v-row justify="center" class="images-project">
+											<v-col class="d-flex child-flex" cols="6">
+												<v-img src="../assets/project_painel.png" aspect-ratio="1" class="lighten-2" contain>
+													<template v-slot:placeholder>
+														<v-row class="fill-height ma-0" align="center" justify="center">
+															<v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+														</v-row>
+													</template>
+												</v-img>
+											</v-col>
+											<v-col class="d-flex child-flex" cols="6">
+												<v-img src="../assets/project_painel2.png" class="lighten-2" contain>
+													<template v-slot:placeholder>
+														<v-row align="center" justify="center">
+															<v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+														</v-row>
+													</template>
+												</v-img>
+											</v-col>
+										</v-row>
+										<v-row>
+											<small class="text-center">*Dados ocultados para proteção de dados internos*</small>
+										</v-row>
+									</v-container>
+								</div>
+							</v-dialog>
+						</v-card-title>
+					</v-card>
+				</v-col>
+
+				<!--CFR Agrotehc-->
+				<v-col cols="6">
+					<v-card>
+						<v-img src="../assets/project_agrotehc.png" max-height="125" contain></v-img>
+						<v-card-title class="text-h6">
+							<v-dialog v-model="dialog1" fullscreen hide-overlay transition="dialog-bottom-transition">
+								<template v-slot:activator="{ on, attrs }">
+									<div class="data-project">
+										<h5 class="title-project" v-bind="attrs" v-on="on">CFR Agrotech
+											<i class="fas fa-expand-alt fa-sm"></i>
+										</h5>
+										<p class="text-project">Aplicativo para controle de finanças Rurais (em desenvolvimento)</p>
+									</div>
+								</template>
+								<div class="content-post-project-father">
+									<v-toolbar dark color="#e59447">
+										<v-btn icon dark @click="dialog1 = false">
+											<v-icon>mdi-close</v-icon>
+										</v-btn>
+									</v-toolbar>
+									<v-container>
+										<v-row justify="center">
+											<v-col cols="7">
+
+												<div class="d-flex justify-content-around content-post-project">
+													<div>
+														<h4>Controle de Finanças Agrícolas</h4>
+														<div class="ml-2">
+															<h6 class="d-flex">
+																<i class="fas fa-laptop"></i>&nbsp;Windows/MacOS/Linux/iOS/Android
+															</h6>
+															<h6 class="d-flex">
+																<i class="far fa-keyboard"></i>&nbsp;Laravel, Vue.js, PHP
+															</h6>
+															<h6 class="d-flex">
+																<i class="fas fa-external-link-alt"></i>&nbsp;localhost
+															</h6>
+														</div>
+													</div>
+													<div class="text-end about-project">
+														<p>O aplicativo tem como propósito dar possiblidade para que produtores rurais possam cadastrar dados em suas safras de cada lavoura, para que possa ter um conhecimento maior sobre gastos, e ganhos em um único histórico, simples e funcional. O projeto ainda está em desenvolvimento e tem como ideia central o aprendizado do desenvolvedor.</p>
+													</div>
+												</div>
+											</v-col>
+										</v-row>
+										<v-row justify="center" class="images-project">
+											<v-col class="d-flex child-flex" cols="6">
+												<v-img src="../assets/project_agrotehc2.png" aspect-ratio="1" class="lighten-2" contain>
+													<template v-slot:placeholder>
+														<v-row class="fill-height ma-0" align="center" justify="center">
+															<v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+														</v-row>
+													</template>
+												</v-img>
+											</v-col>
+											<v-col class="d-flex child-flex" cols="6">
+												<v-img src="../assets/project_agrotehc3.png" class="lighten-2" contain>
+													<template v-slot:placeholder>
+														<v-row align="center" justify="center">
+															<v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+														</v-row>
+													</template>
+												</v-img>
+											</v-col>
+										</v-row>
+<!--										<v-row>-->
+<!--											<small class="text-center">*Dados ocultados para proteção de dados internos*</small>-->
+<!--										</v-row>-->
+
+									</v-container>
+								</div>
+							</v-dialog>
+						</v-card-title>
+					</v-card>
+				</v-col>
+
+
+			</v-row>
+		</v-container>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			dialog: false,
+			dialog1: false,
+			notifications: false,
+			sound: true,
+			widgets: false,
+		}
+	},
+	name: "Portfolio"
+
+}
+
+</script>
+
+<style scoped>
+.data-project {
+	line-height: 1;
+	text-align: center;
+}
+
+.text-project {
+	font-size: 16px !important;
+	font-weight: normal;
+
+}
+
+.content-post-project-father {
+	background-color: white;
+	min-height: 100vh;
+}
+
+.content-post-project {
+	margin-top: 30px;
+}
+
+i {
+	color: var(--laranja);
+}
+
+.fa-expand-alt {
+	color: black;
+}
+
+.about-project {
+	margin-left: 200px;
+}
+
+.images-project {
+	margin-top: -100px;
+}
+
+</style>
